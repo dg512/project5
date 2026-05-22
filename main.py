9fa56687b96c883dbae1dc8ec9bb2573d8c778fd
dg512 <dg2026512@danggok.hs.kr>
2026-05-22 12:11:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+Web VPython 3.2
 
+import random
+sphere ()
+s = []
+for i in range(20) : 
+    s.append(sphere(pos = vec(random.uniform(-10,10),random.uniform(-10,10),random.uniform(-10,10))))
+
+while True :
+    rate(30)
+    k = keysdown()
+    r = random.randint(0,19)
+    
+    for i in range(20):
+        s[i].pos.x += random.uniform(-1, 1)
+        s[i].pos.y += random.uniform(-1, 1)
+        s[i].pos.z += random.uniform(-1, 1)
+
+    if ' ' in k :
+        rate(20)
+        for i in range(20): 
+            if i == r : 
+                s[i].color = color.red
+            else : 
+                s[i].color = color.white
