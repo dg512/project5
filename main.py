6d9ad58e667d5f901fd933e8957999dd8883891d
dg512 <dg2026512@danggok.hs.kr>
2026-06-09 12:11:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 Web VPython 3.2
 
 import random
-sphere ()
+
 s = []
+u = [] 
+
 for i in range(20) : 
-    s.append(sphere(pos = vec(random.uniform(-10,10),random.uniform(-10,10),random.uniform(-10,10))))
+    s.append(sphere(pos = vec(random.uniform(-10,10),random.uniform(-10,10),0)))
+
 
 while True :
+    
     rate(100)
     k = keysdown()
     r = random.randint(0,19)
+    
     if ' ' in k : 
         s[0].color = color.red
         
@@ -17,11 +22,27 @@ while True :
         u.append(s[r])
         t = len(u)
         p = random.randint(0,t-1)
+        
         for x in s :
             if (  (u[p].pos.x- x.pos.x)**2  +  (u[p].pos.y- x.pos.y)**2 )**0.5 < 2  : 
                 x.color = color.red
-    for x in s :
-        if -10 < x.pos.x < 10 :
-            x.pos.x += random.uniform(-0.2, 0.2)
-        if -10 < x.pos.y < 10 :
-            x.pos.y += random.uniform(-0.2, 0.2)
+    if 'h' in k :
+        for x in s :
+            if -10 < x.pos.x < 10 :
+                x.pos.x += random.uniform(-0.2, 0.2)
+            if -10 < x.pos.y < 10 :
+                x.pos.y += random.uniform(-0.2, 0.2)
+        print("사회적 거리두기가 시행됩니다.")
+   
+    else:    
+        for x in s:  
+            if -10 < x.pos.x < 10 :  
+                x.pos.x += random.uniform(-0.4, 0.4)
+            if -10 < x.pos.y < 10 :  
+                x.pos.y += random.uniform(-0.4, 0.4)      
+        print(" ")
+    
+
+            
+        
+    
